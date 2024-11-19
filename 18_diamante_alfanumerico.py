a0465cf097aa36a2b38140708e45ca1b7b20a779
PabloChorda <pablochorda365@gmail.com>
2024-11-19 23:42:10 +0100

    Crea una función que dibuje un diamante alfanumérico

--- 18_diamante_alfanumerico.py
+++ 18_diamante_alfanumerico.py
@@ -10,3 +10,28 @@ Cada línea está centrada con espacios iniciales para formar un diamante
 perfecto.
 
 '''
+
+
+def diamante_alfanumerico(n):
+    for i in range(1, n + 1):
+        # Espacios iniciales para centrar
+        espacios = " " * (n - i)
+        # Construir la línea de números y letras
+        numeros = "".join(str(x) for x in range(1, i))
+        letras = "".join(chr(65 + j) for j in range(i - 1))
+        # Línea completa
+        linea = numeros + letras + numeros[::-1]
+        print(espacios + "1" + linea)
+    
+    for i in range(n - 1, 0, -1):
+        # Espacios iniciales para centrar
+        espacios = " " * (n - i)
+        # Construir la línea de números y letras
+        numeros = "".join(str(x) for x in range(1, i))
+        letras = "".join(chr(65 + j) for j in range(i - 1))
+        # Línea completa
+        linea = numeros + letras + numeros[::-1]
+        print(espacios + "1" + linea)
+
+# Llama a la función con un número final
+diamante_alfanumerico(5)
