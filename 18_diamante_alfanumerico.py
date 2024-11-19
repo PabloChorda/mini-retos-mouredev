b10a683d5c560364d8473d31da53d2a94f4dbb6c
PabloChorda <pablochorda365@gmail.com>
2024-11-19 23:40:24 +0100

    diamante alfa

---
+++ 18_diamante_alfanumerico.py
@@ -0,0 +1,12 @@
+'''
+RETO DIAMANTE ALFANUMÉRICO
+Crea una función que dibuje un diamante alfanumérico 
+basado en un número n de líneas.
+
+En la parte superior, cada línea contiene números y letras alternados, 
+comenzando desde "1" y "A", creciendo hacia los lados.
+En la parte inferior, el patrón se invierte.
+Cada línea está centrada con espacios iniciales para formar un diamante 
+perfecto.
+
+'''
