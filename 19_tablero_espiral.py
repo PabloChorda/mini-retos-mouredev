8b57ea39d6895dce3b1b138205756445907f53b2
PabloChorda <pablochorda365@gmail.com>
2024-11-19 23:48:56 +0100

    19 reto

---
+++ 19_tablero_espiral.py
@@ -0,0 +1,8 @@
+'''
+RETO TABLERO EN ESPIRAL
+Crea una función que genere un tablero numérico en forma de espiral, 
+partiendo desde el número 1 en el centro y avanzando hacia afuera en 
+sentido horario.
+
+'''
+
