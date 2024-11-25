8c3f73e96775d91789acb03adb5067dcccac34c8
PabloChorda <pablochorda365@gmail.com>
2024-11-25 23:11:30 +0100

    Simula una batalla de dados

--- 20_batalla_de_dados.py
+++ 20_batalla_de_dados.py
@@ -2,8 +2,9 @@
 Crea una función que simule una batalla entre dos jugadores lanzando dados. Ambos jugadores tienen un número inicial de puntos, y el juego funciona de la siguiente manera:
 
 Cada jugador lanza dos dados (valores aleatorios entre 1 y 6).
-El jugador con el resultado más alto en su turno le quita puntos al otro jugador, igual a la diferencia entre los totales de los dados.
-Si hay empate, no se pierde ni se gana puntos en ese turno.
+El jugador con el resultado más alto en su turno le quita puntos al otro jugador, 
+igual a la diferencia entre los totales de los dados.
+Si hay empate, no se pierde ni se gana puntos en el turno.
 El juego continúa hasta que uno de los jugadores tenga 0 puntos o menos.
 Muestra el ganador y cada turno del juego.
 '''
